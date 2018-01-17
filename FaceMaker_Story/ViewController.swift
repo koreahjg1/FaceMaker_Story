d8d4e1bcf2c588ea246d0aab96aefa59501b8f61
Jonggi Hong <jonggihong@jonggis-mbp.fios-router.home>
2018-01-16 20:11:47 -0500

    Initial Commit

---
+++ FaceMaker_Story/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceMaker_Story
+//
+//  Created by Jonggi Hong on 1/16/18.
+//  Copyright © 2018 Jonggi Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FaceMaker_StoryTests/FaceMaker_StoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  FaceMaker_StoryTests.swift
+//  FaceMaker_StoryTests
+//
+//  Created by Jonggi Hong on 1/16/18.
+//  Copyright © 2018 Jonggi Hong. All rights reserved.
+//
+
+import XCTest
+@testable import FaceMaker_Story
+
+class FaceMaker_StoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
